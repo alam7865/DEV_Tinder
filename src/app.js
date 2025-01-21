71a722da1910cb84906f0cba34195bef9f47f028
alam7865 <sabbajalam5@gmail.com>
2025-01-21 20:34:11 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello ALAM BHAI");
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Akshay",
+    LastName: "Sani",
+  });
 });
+
+///////////////// Post ////////////////////
+app.post("/user", (req, res) => {
+  res.send("Data updated Succesfully");
+});
+
+//////////////// Data deleted /////////////////
+app.delete("/user", (req, res) => {
+  res.send("Data Delteded Succesfully");
+});
+
+app.use("/people", (req, res) => {
+  res.send("Hello People");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("Hello ALAM BHAI");
+// });
+
 app.listen(3000, () => {
   console.log("Server Started at port 3000");
 });
